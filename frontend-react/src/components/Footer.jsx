3e824c97214e2bb52f626b61f45ccf82889e698a
codewithyashsoni <yashsoni.contact@gmail.com>
2026-06-29 21:33:29 +0530

    A small change

--- frontend-react/src/components/Footer.jsx
+++ frontend-react/src/components/Footer.jsx
@@ -4,17 +4,13 @@ const Footer = () => {
   return (
     <>
     <footer className="footer text-center py-1">
-
-
-    <h6 className="text-info fw-bold mb-2">
-        📈 StockVision AI
-    </h6>
-
-    <p className="text-light mb-1">
-        Developed by Yash Soni • Mahesh Suyal
-    </p>
-
-</footer>
+      <h6 className="text-info fw-bold mb-2">
+          📈 StockVision AI
+      </h6>
+      <p className="text-light mb-1">
+          Developed by Yash Soni • Mahesh Suyal
+      </p>
+    </footer>
     </>
   )
 }
